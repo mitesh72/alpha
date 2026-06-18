d04c08862b57da9b6807ded1716c145815f5124b
unknown <uniyalmitesh812@gmail.com>
2026-06-18 17:11:31 +0530

    Added dseeded products

--- config.js
+++ config.js
@@ -1,19 +1,22 @@
 import mongoose from "mongoose";
- mongoose.connect(process.env.MONGODB_URI)
-    .then(() => console.log("database connected"))
-    .catch((err) => console.log("database connection error:", err.message));
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log("database connected"))
+  .catch((err) => console.log("database connection error:", err.message));
 
 const LoginSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    unique: true  // prevents duplicate usernames
+    unique: true
   },
   password: {
     type: String,
     required: true
   },
-   role: {                    
+  role: {
     type: String,
     enum: ["user", "admin"],
     default: "user"
